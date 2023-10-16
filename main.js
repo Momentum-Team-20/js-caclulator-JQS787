c0228bfd511e20253c169e0f0c0ed09f36ae68e7
Jose <qjoseantonio@gmail.com>
2023-10-15 22:48:18 -0700

    cal done

--- main.js
+++ main.js
@@ -1,1 +1,53 @@
-console.log("main.js");
+const screentop = document.querySelector(".top");
+const botones = document.querySelectorAll(".num")
+
+botones.forEach(boton => {
+    boton.addEventListener("click" ,() => {
+        const hitBoton = boton.textContent;
+        
+        if (boton.id ==="c") {
+            screentop.textContent = "";
+            return;
+        
+        }
+        if(boton.id === "equal") {
+        screentop.textContent = eval(screentop.textContent);
+        return;
+
+        }
+        
+        if(screentop.textContent === ""){
+
+        }
+        screentop.textContent += hitBoton
+        
+        
+         
+        })
+
+       
+    }) 
+
+        
+        
+       
+
+
+      
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
